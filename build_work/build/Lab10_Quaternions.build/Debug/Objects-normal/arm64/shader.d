9756c083e5862aa20492d5fd5ae2ffb14acdf543
Jon Shiach <jon.shiach@gmail.com>
2024-04-21 19:09:33 +0100

    First commit

--- build_work/build/Lab10_Quaternions.build/Debug/Objects-normal/arm64/shader.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/jon/Library/CloudStorage/Dropbox/Work/2023_2024/Graphics/Git\ repos/Lab10_Quaternions/source/shader.cpp \
-  /Users/jon/Library/CloudStorage/Dropbox/Work/2023_2024/Graphics/Git\ repos/Lab10_Quaternions/external/glew-1.13.0/include/GL/glew.h \
-  /Users/jon/Library/CloudStorage/Dropbox/Work/2023_2024/Graphics/Git\ repos/Lab10_Quaternions/source/shader.hpp
